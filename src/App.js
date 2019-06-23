e0817abb219bb2b90ddf5c72d4f8e92301503939
pcmnac <pcmnac@gmail.com>
2019-06-23 13:38:02 +0100

    ♻️ Using functional component in the example

--- src/App.js
+++ src/App.js
@@ -17,70 +17,69 @@ import './App.css';
 const { duration } = media;
 const { orientation, locale } = config;
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>DSPLAY Template</h1>
-                <h2>Raw Values</h2>
-                <div>
-                    <p>
-                        Media:
-                        <pre>{JSON.stringify(media, null, 4)}</pre>
-                    </p>
-                    <p>
-                        Config:
-                        <pre>{JSON.stringify(config, null, 4)}</pre>
-                    </p>
-                    <p>
-                        Template:
-                        <pre>{JSON.stringify(template, null, 4)}</pre>
-                    </p>
-                </div>
-                <h2>Media Values Examples</h2>
-                <div>
-                    <p>
-                        Duration:
-                        <span class="val">{duration}</span>
-                    </p>
-                </div>
-                <h2>Configuration Values Examples</h2>
-                <div>
-                    <p>
-                        Orientation:
-                        <span class="val">{orientation}</span>
-                    </p>
-                    <p>
-                        Locale:
-                        <span class="val">{locale}</span>
-                    </p>
-                </div>
-                <h2>Custom Template Var Examples</h2>
-                <div>
-                    <p>
-                        String:
-                        <span class="val">{tval('title', 'Default Value')}</span>
-                    </p>
-                    <p>
-                        Boolean:
-                        <span class="val">{tbval('expanded', true) ? 'Yes' : 'No'}</span>
-                    </p>
-                    <p>
-                        Int:
-                        <span class="val">{tival('page_size', 10)}</span>
-                    </p>
-                    <p>
-                        Double:
-                        <span class="val">{tfval('rate', .75)}</span>
-                    </p>
-                    <p>
-                        Vertical?:
-                        <span class="val">{isVertical ? 'Yes' : 'No'}</span>
-                    </p>
-                </div>
+function App() {
+    return (
+        <div className="App">
+            <h1>DSPLAY Template</h1>
+            <h2>Raw Values</h2>
+            <div>
+                <p>
+                    Media:
+                    <pre>{JSON.stringify(media, null, 4)}</pre>
+                </p>
+                <p>
+                    Config:
+                    <pre>{JSON.stringify(config, null, 4)}</pre>
+                </p>
+                <p>
+                    Template:
+                    <pre>{JSON.stringify(template, null, 4)}</pre>
+                </p>
             </div>
-        );
-    }
+            <h2>Media Values Examples</h2>
+            <div>
+                <p>
+                    Duration:
+                    <span class="val">{duration}</span>
+                </p>
+            </div>
+            <h2>Configuration Values Examples</h2>
+            <div>
+                <p>
+                    Orientation:
+                    <span class="val">{orientation}</span>
+                </p>
+                <p>
+                    Locale:
+                    <span class="val">{locale}</span>
+                </p>
+            </div>
+            <h2>Custom Template Var Examples</h2>
+            <div>
+                <p>
+                    String:
+                    <span class="val">{tval('title', 'Default Value')}</span>
+                </p>
+                <p>
+                    Boolean:
+                    <span class="val">{tbval('expanded', true) ? 'Yes' : 'No'}</span>
+                </p>
+                <p>
+                    Int:
+                    <span class="val">{tival('page_size', 10)}</span>
+                </p>
+                <p>
+                    Double:
+                    <span class="val">{tfval('rate', .75)}</span>
+                </p>
+                <p>
+                    Vertical?:
+                    <span class="val">{isVertical ? 'Yes' : 'No'}</span>
+                </p>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
