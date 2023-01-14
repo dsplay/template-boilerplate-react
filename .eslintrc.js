af5f3a72f1d807ab5a53879a28e89238e707d899
Paulo Coutinho <pcmnac@gmail.com>
2023-01-14 15:12:57 +0000

    🐛 fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,14 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: '@babel/eslint-parser',
+  // parser: '@babel/eslint-parser',
+
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false, // <== ADD THIS
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
+  },
 
   plugins: [
     'jsx-a11y',
